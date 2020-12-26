bf6bcc2e401cde2e75df596bfad1bb1d890178b7
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-27 02:23:52 +0300

    0406-styling-element

--- src/button.js
+++ src/button.js
@@ -1,6 +1,13 @@
 const buttonTemplate = document.createElement("template");
 buttonTemplate.innerHTML = /* html */ `
 <style>
+  :host {
+    display: block;
+  }
+  :host([inprogress]) {
+    transform: scale(1.1);
+    transform-origin: top left;
+  }
   .btn {
     background-color: #0070f3;
     color: white;
