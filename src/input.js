3a27c11c146eefd77c4c1cd06a5cef1545f1601a
Basar Buyukkahraman <basarbk@yahoo.com>
2020-12-29 20:50:24 +0300

    0601-input-element-validation

--- src/input.js
+++ src/input.js
@@ -10,9 +10,30 @@ template.innerHTML = /* html */ `
     border: 1px solid lightgray;
     padding: 10px;
   }
+  span {
+    font-size: 0.8rem;
+    display: none;
+  }
+  :host([validation="invalid"]) span {
+    display: block;
+    color: red;
+  }
+
+  :host([validation="invalid"]) input {
+    border-color: red;
+  }
+  :host([validation="valid"]) span {
+    display: block;
+    color: green;
+  }
+
+  :host([validation="valid"]) input {
+    border-color: green;
+  }
 </style>
 <label></label>
 <input>
+<span>Message</span>
 `;
 
 class Input extends HTMLElement {
@@ -21,10 +42,30 @@ class Input extends HTMLElement {
     this.attachShadow({ mode: "open" });
   }
 
+  get help() {
+    return this.getAttribute("help");
+  }
+
+  set help(help) {
+    this.setAttribute("help", help);
+  }
+
+  get validation() {
+    return this.getAttribute("validation");
+  }
+
+  set validation(validation) {
+    this.setAttribute("validation", validation);
+  }
+
   connectedCallback() {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     const label = this.shadowRoot.querySelector("label");
     label.textContent = this.getAttribute("label");
+
+    this.span = this.shadowRoot.querySelector("span");
+    this.span.textContent = this.getAttribute("help");
+
     const input = this.shadowRoot.querySelector("input");
     input.addEventListener("input", (event) => {
       event.stopPropagation();
